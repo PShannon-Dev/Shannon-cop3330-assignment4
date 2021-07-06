016ca86f78149e65e57842e7254545618783b1ca
paulshannon-coding <mikey4295@hotmail.com>
2021-07-05 23:59:50 -0400

    part1

---
+++ src/main/java/ucf/assignments/List.java
@@ -0,0 +1,16 @@
+package ucf.assignments;
+
+import java.util.ArrayList;
+
+public class List {
+    private String listTitle;
+    private ArrayList <Item> ToDoList;
+
+    public String getListTitle() {
+        return listTitle;
+    }
+
+    public void setListTitle(String listTitle) {
+        this.listTitle = listTitle;
+    }
+}
---
+++ src/main/java/ucf/assignments/ToDoListController.java
@@ -0,0 +1,32 @@
+package ucf.assignments;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+import java.awt.event.ActionEvent;
+
+public class ToDoListController {
+
+    @FXML
+    private ListView<List> toDoLists;
+
+    @FXML
+    private TextField listName;
+
+    public void remove_selected_list(javafx.event.ActionEvent actionEvent) {
+        //select multiple using listOfLists.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        //remove selected lists from the json file
+    }
+
+    public void add_list(javafx.scene.input.MouseEvent mouseEvent) {
+        //set list name to the textfield entry listName
+    }
+
+    public void open_list(MouseEvent mouseEvent) {
+        //new scene
+        //display new modal window List
+    }
+}
+
